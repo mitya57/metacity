b8b543540fe9dce61a1cf0c16999cb3ce5a92be4
Iain Holmes <iain@src.gnome.org>
2007-12-05 01:17:00 +0000

    Fix the non compositor build
    svn path=/branches/iains-blingtastic-bucket-o-bling/; revision=3452

--- src/screen.c
+++ src/screen.c
@@ -818,8 +818,10 @@ meta_screen_manage_all_windows (MetaScreen *screen)
                                            &info->attrs);
       if (info->xwindow == screen->no_focus_window ||
           info->xwindow == screen->flash_window ||
-          info->xwindow == screen->wm_sn_selection_window ||
-          info->xwindow == screen->wm_cm_selection_window) {
+#ifdef HAVE_COMPOSITE_EXTENSIONS
+          info->xwindow == screen->wm_cm_selection_window ||
+#endif
+          info->xwindow == screen->wm_sn_selection_window) {
         g_print ("Not managing our own windows\n");
         continue;
       }
